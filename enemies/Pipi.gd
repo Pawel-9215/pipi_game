d0ae73c55937599c1a32a27510cc0a7bbb018fd2
Paweł Hordyniak <hordyniak@protonmail.com>
2021-10-17 08:25:23 +0200

    started with pipi himself

---
+++ enemies/Pipi.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+# states for statemachine:
+# Patrol, pursuit, attack, numb
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
